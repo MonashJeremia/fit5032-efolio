22b4628440fd866ffa0f78df179735a0d0e3e27f
Jeremia <jeremiayo@gmail.com>
2025-08-01 17:36:48 +1000

    add MyName component to the wrapper in App.vue

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,7 @@ import MyName from './components/MyName.vue';
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
+      <MyName />
       <HelloWorld msg="You did it!" />
     </div>
   </header>
