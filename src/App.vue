735de9ba65285a832bea3f544aee184ca9c988d1
Jeremia <jeremiayo@gmail.com>
2025-08-01 16:33:59 +1000

    add MyName component and import it in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
 import TheWelcome from './components/TheWelcome.vue'
+import MyName from './components/MyName.vue';
+
 </script>
 
 <template>
---
+++ src/components/MyName.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <b class="large-text">My name is Jeremia Yovinus.</b>
+    </div>
+</template>
+
+<style scoped>
+.large-text {
+    font-size: 1.5rem;
+}
+</style>
