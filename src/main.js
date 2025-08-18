229ded0f026988deebc9e0876f9148c2b97e1321
Jeremia Yovinus <jyov0001@student.monash.edu>
2025-08-18 16:54:42 +1000

    add Bootstrap dependency and update styles for improved UI

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
-import './assets/main.css'
-import './style.css'
-
 import { createApp } from 'vue'
 import App from './App.vue'
+
+import './assets/main.css'
+// Import Bootstrap CSS
+import 'bootstrap/dist/css/bootstrap.min.css'
+// import './style.css'
 
 createApp(App).mount('#app')
