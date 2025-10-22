2d908756ec4e852d1c37e7a7ae1dd20b3470e136
Jeremia Yovinus <jyov0001@student.monash.edu>
2025-10-22 16:06:53 +1100

    feat: fixing minor issue

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,12 @@
-module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/<fit5032-efolio>/' : '/'
+/* eslint-env node */
+const isProd =
+  typeof process !== 'undefined' && process.env && process.env.NODE_ENV === 'production'
+const config = {
+  publicPath: isProd ? '/fit5032-efolio/' : '/'
 }
+
+if (typeof module !== 'undefined' && module.exports) {
+  module.exports = config
+}
+
+export default config
